f6dce9089eeebefa0cde00d234161557f1b47330
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2020-12-22 22:58:53 +0100

    Version 1.3.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='cf_submit',
-    version='1.3.8',
+    version='1.3.9',
     scripts=['cf'],
     author='Nasreddine Bac Ali',
     author_email='nasreddine.bacali95@gmail.com',
